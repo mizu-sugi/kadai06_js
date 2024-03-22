a4a2588dca1dddacb791ba24244e1de03b532233
Mizu-sugi <dnnuggets_15@yahoo.co.jp>
2024-03-22 17:32:09 +0900

    キャッシュ削除

--- js/APIkey.js
+++
@@ -1,4 +0,0 @@
-const APIkey = "AsHfQjRtMr14JXw3YF4d1_Eyq5aq6dSZ10egPn8Q6cHrQGaUOAKycLTt2nYDtwyp";
-
-export { APIkey };
-
--- js/firebase.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyA2UHHc05KrYd-gZddMO3EU8OUFZGUoQ_s",
-    authDomain: "trip-to-minnesota.firebaseapp.com",
-    databaseURL: "https://trip-to-minnesota-default-rtdb.firebaseio.com",
-    projectId: "trip-to-minnesota",
-    storageBucket: "trip-to-minnesota.appspot.com",
-    messagingSenderId: "528127136005",
-    appId: "1:528127136005:web:59141a804702ecaaf4e76c"
-  };
